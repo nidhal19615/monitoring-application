a50e64f996e4427244fa38ed9946767c7e8fc87b
nidhal19615 <nidhal.sassi@esprit.tn>
2024-06-10 18:25:55 +0200

    ooooopp

--- src/Kc.js
+++ src/Kc.js
@@ -1,9 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const Kc = new Keycloak({
-  //   url: "http://localhost:8890",
-  url: "http://10.224.0.29:8443",
-  // url: "http://98.66.206.49:80/",
+  url: "http://10.224.0.29:8080/auth",
 
   realm: "cost-dashbord",
   clientId: "react-app",
