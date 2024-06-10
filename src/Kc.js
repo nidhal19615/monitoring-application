f001c303dcbe3a0f1ce9b9e0b020798f7620a947
nidhal19615 <nidhal.sassi@esprit.tn>
2024-06-10 16:44:21 +0200

    oooo

--- src/Kc.js
+++ src/Kc.js
@@ -3,7 +3,7 @@ import Keycloak from "keycloak-js";
 const Kc = new Keycloak({
   ///  url: "http://localhost:8890",
   // url: "http://10.0.141.223:31503",
-  url: "http://98.66.206.49:80/auth",
+  url: "http://98.66.206.49:80/",
   realm: "cost-dashbord",
   clientId: "react-app",
 });
