26cfcea1d9b9bbaab46d10414e9dca098749ebb2
nidhal19615 <nidhal.sassi@esprit.tn>
2024-06-18 12:00:55 +0200

    second---commit

--- src/Kc.js
+++ src/Kc.js
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const kcConfig = {
-  url: "http://98.66.206.49/auth/", // /Replace with your actual base URL
+  url: "http://98.66.206.49:80/realms/your-realm/protocol/openid-connect/auth?client_id=react-app&response_type=code&scope=openid&redirect_uri=http%3A%2F%2Fmy-redirect-url.com%2Fcallback&state=randomState123",
   realm: "cost-dashbord", // Replace with your actual realm name
   clientId: "react-app",
 };
