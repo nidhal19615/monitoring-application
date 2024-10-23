361785be6b1a3a960f236d46455ec011ff748997
nidhal19615 <nidhal.sassi@esprit.tn>
2024-10-23 12:59:36 +0200

    ooo

--- src/Kc.js
+++ src/Kc.js
@@ -4,8 +4,8 @@ const Kc = new Keycloak({
   /// url: "http://10.224.0.29:8080/auth",
   // url: "http://10.224.0.29:8443/auth",
 
-   url: "http://localhost:8890/",
-  //url: "http://98.66.206.49/auth", 
+  // url: "http://localhost:8890/",
+  url: "http://98.66.206.49/auth", 
 
   realm: "cost-dashbord",
   clientId: "react-app",
